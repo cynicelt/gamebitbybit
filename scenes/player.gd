0a2fd28955e25fa64a6e2c159b65f6e2555c7b76
PAUL KEANE <paul.keane@britinsurance.com>
2025-03-11 09:01:56 +0000

    slime collision fix, player anim WIP

--- scenes/player.gd
+++ scenes/player.gd
@@ -11,7 +11,6 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -28,9 +27,9 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.flip_h = false
 		elif direction < 0:
 			animated_sprite.flip_h = true
-			
 		velocity.x = direction * SPEED
 		animated_sprite.play('move')
+		
 	elif not is_on_floor():
 		if direction > 0:
 			animated_sprite.flip_h = false
@@ -40,6 +39,5 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.play('roll')
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animated_sprite.play('default')
 
 	move_and_slide()
